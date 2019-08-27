61d883779f404bac48b44d1a4b66cf8172577316
Ellen Lee <leechinghui.tw@gmail.com>
2019-08-27 13:30:17 +0800

    Finish index action and page

--- app.js
+++ app.js
@@ -22,12 +22,15 @@ const Todo = require('./models/todo')
 
 // 首頁
 app.get('/', (req, res) => {
-  return res.render('index')
+  Todo.find((err, todos) => {
+    if (err) return console.error(err)
+    return res.render('index', { todos: todos })
+  })
 })
 
 // 列出全部 Todo
 app.get('/todos', (req, res) => {
-  res.send('列出所有 Todo')
+  return res.redirect('/')
 })
 
 // 新增一筆 Todo 頁面
