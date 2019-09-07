f28b930eeaee6b97290f6d5bbf57d5a1cfade081
Ellen Lee <leechinghui.tw@gmail.com>
2019-09-07 11:39:26 +0800

    Install express-session

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -21,6 +22,12 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 })
+
+app.use(session({
+  secret: 'your secret key', // secret: 定義一組屬於你的字串做為私鑰
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use('/', require('./routes/home'))
 app.use('/todos', require('./routes/todo'))
