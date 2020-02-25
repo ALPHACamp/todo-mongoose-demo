8c1ee7eaf3ea143000a21b1e298f2f6c0d63877d
Ellen Lee <leechinghui.tw@gmail.com>
2020-02-25 20:15:35 +0800

    Add user authentication in todo and home routers

--- routes/todo.js
+++ routes/todo.js
@@ -2,18 +2,20 @@ const express = require('express')
 const router = express.Router()
 const Todo = require('../models/todo')
 
+const { authenticated } = require('../config/auth')
+
 // 設定路由
 // 列出全部 Todo
-router.get('/', (req, res) => {
+router.get('/', authenticated, (req, res) => {
   return res.redirect('/')
 })
 
 // 新增一筆 Todo 頁面
-router.get('/new', (req, res) => {
+router.get('/new', authenticated, (req, res) => {
   return res.render('new')
 })
 // 顯示一筆 Todo 的詳細內容
-router.get('/:id', (req, res) => {
+router.get('/:id', authenticated, (req, res) => {
   Todo.findById(req.params.id)
     .lean()
     .exec((err, todo) => {
@@ -22,7 +24,7 @@ router.get('/:id', (req, res) => {
     })
 })
 // 新增一筆  Todo
-router.post('/', (req, res) => {
+router.post('/', authenticated, (req, res) => {
   // 建立 Todo model 實例
   const todo = new Todo({
     name: req.body.name // name 是從 new 頁面 form 傳過來
@@ -34,7 +36,7 @@ router.post('/', (req, res) => {
   })
 })
 // 修改 Todo 頁面
-router.get('/:id/edit', (req, res) => {
+router.get('/:id/edit', authenticated, (req, res) => {
   Todo.findById(req.params.id)
     .lean()
     .exec((err, todo) => {
@@ -43,7 +45,7 @@ router.get('/:id/edit', (req, res) => {
     })
 })
 // 修改 Todo
-router.put('/:id', (req, res) => {
+router.put('/:id', authenticated, (req, res) => {
   Todo.findById(req.params.id, (err, todo) => {
     if (err) return console.error(err)
     todo.name = req.body.name
@@ -59,7 +61,7 @@ router.put('/:id', (req, res) => {
   })
 })
 // 刪除 Todo
-router.delete('/:id/delete', (req, res) => {
+router.delete('/:id/delete', authenticated, (req, res) => {
   Todo.findById(req.params.id, (err, todo) => {
     if (err) return console.error(err)
     todo.remove(err => {
