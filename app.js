20e5b67d957a30d22efd793782bf002cc234c652
Ellen Lee <leechinghui.tw@gmail.com>
2020-02-25 13:32:55 +0800

    clear comment in app.js

--- app.js
+++ app.js
@@ -2,45 +2,28 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose') // 載入 mongoose
 
-// 引用 express-handlebars
 const exphbs = require('express-handlebars')
-
-// 引用 body-parser
 const bodyParser = require('body-parser')
-
-// 引用 method-override
 const methodOverride = require('method-override')
 
-// 設定 bodyParser
-app.use(bodyParser.urlencoded({ extended: true }))
-
-// 告訴 express 使用 handlebars 當作 template engine 並預設 layout 是 main
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
-// 設定 method-override
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-// 資料庫連線
 mongoose.connect('mongodb://localhost/todo', { useNewUrlParser: true, useUnifiedTopology: true })
 
-// 資料庫連線後，透過 mongoose.connection 拿到 Connection 的物件
 const db = mongoose.connection
 
-// 連線異常
 db.on('error', () => {
   console.log('mongodb error!')
 })
 
-// 連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-// 載入 todo model
-const Todo = require('./models/todo')
 
-// 載入路由器
 app.use('/', require('./routes/home'))
 app.use('/todos', require('./routes/todo'))
 app.use('/users', require('./routes/user'))
